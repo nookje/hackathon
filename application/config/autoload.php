c436b4cdb241962b695bd6e657af2d09b2fba413
Razvan Smarandeanu <razvansg@yahoo.com>
2014-06-13 18:29:26 +0300

    htaccess

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('translations', 'authorization');
+$autoload['libraries'] = array('authorization');
 
 
 /*
