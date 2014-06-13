fca15f9787f17574d1c867941fe9fb6bfcce18f8
Razvan Smarandeanu <razvansg@yahoo.com>
2014-06-14 00:19:30 +0300

    requests filter

--- application/controllers/requests.php
+++ application/controllers/requests.php
@@ -8,16 +8,8 @@ class Requests extends CI_Controller {
 
 		$this->load->model('requests_model', 'requests');		
 
-		$direction 		= $this->input->get('direction', true);
-		$reference 		= $this->input->get('reference', true);
-		$id 			= $this->input->get('id', true);
 
-		if ($direction && $reference && $id) {
-			$result = $this->requests->loadRequests($direction, $reference, $id);
-		} else {
-			$result = $this->requests->loadRequests();
-		}
-
+		$result = $this->requests->loadRequests();
 
 
 		echo json_encode($result);
--- application/models/requests_model.php
+++ application/models/requests_model.php
@@ -7,8 +7,35 @@ class Requests_model extends CI_Model {
         parent::__construct();
     }    
     
-    function loadRequests($direction = '', $reference = '', $id = '')
+    function loadRequests()
     {
+    
+		$direction 		= $this->input->get('direction', true);
+		$reference 		= $this->input->get('reference', true);
+		$id 			= $this->input->get('id', true);
+
+		$status 		= $this->input->get('status', true);
+		$supplier 		= $this->input->get('supplier', true);
+		$supplier_type 	= $this->input->get('supplier_type', true);
+		$location 		= $this->input->get('location', true);
+
+    	$condition = '';
+    	if ($status) {
+    		$condition .= " AND status = '{$status}' ";
+    	}
+
+    	if ($supplier) {
+    		$condition .= " AND supplier = '{$supplier}' ";
+    	}
+
+    	if ($supplier_type) {
+    		$condition .= " AND supplier_type = '{$supplier_type}' ";
+    	}
+
+    	if ($location) {
+    		$condition .= " AND location = '{$location}' ";
+    	}
+
     	if ($direction && $reference && $id) {
     		if ($direction == '<') {
     			$order = 'DESC';
@@ -21,6 +48,7 @@ class Requests_model extends CI_Model {
 					FROM requests
 					WHERE delivery_date {$direction}= '{$reference}' 
 					AND id != {$id}
+					{$condition}
 					ORDER BY delivery_date {$order}
 					LIMIT 3
 			";
@@ -33,6 +61,7 @@ class Requests_model extends CI_Model {
 			$q = "	SELECT * FROM (SELECT delivery_date 
 				FROM requests
 				WHERE delivery_date < now()
+				{$condition}
 				ORDER BY delivery_date DESC
 				LIMIT 3) as T ORDER BY T.delivery_date ASC
 			";
@@ -43,13 +72,14 @@ class Requests_model extends CI_Model {
 			$q = "	SELECT delivery_date 
 				FROM requests
 				WHERE delivery_date >= now()
+				{$condition}
 				ORDER BY delivery_date ASC
 				LIMIT 3
 			";
 
 			$query = $this->db->query($q);
 			$result2 = $query->result_array();
-			
+
 			$result = array_merge($result1, $result2);
     	}
 
